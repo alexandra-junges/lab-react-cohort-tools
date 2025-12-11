582b32603d6b4fb25aeba7d56a7fae8b218ebb96
Alexandra Domareski <aleexandra.junges@gmail.com>
2025-12-11 14:45:03 +0100

    react routers added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import StudentCard from "../components/StudentCard";
+import { Link } from "react-router-dom";
 
 import studentsData from "../assets/students.json";
 
@@ -17,11 +18,18 @@ function HomePage() {
           <span style={{ flexBasis: "20%" }}>Email</span>
           <span style={{ flexBasis: "20%" }}>Phone</span>
         </div>
-
+      
       {students &&
         students.map((student) => {
           return (
-              <StudentCard key={student._id} {...student} />
+            <>
+              <Link 
+                to={`/students/${student._id}`} 
+                key={student._id}
+              >
+                <StudentCard {...student} />
+              </Link>
+            </>
           );
         })}
     </div>
